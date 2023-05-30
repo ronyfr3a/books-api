bcceab4c8a2c1ff41d1be477f24f70f81f93b4e1
ronyfr3a <rakib.fstackdev@gmail.com>
2023-05-30 11:32:12 +0600

    woo

--- server/routes/search.js
+++ server/routes/search.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const Search = require("../controller/search")
 
 router.get("/:key", Search.search)
-router.get("/:id", Search.getOne)
+router.get("/one/:id", Search.getOne)
 
 module.exports = router
